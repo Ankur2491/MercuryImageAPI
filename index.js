63c57fe96b8125e6410eead12c8d16827f505580
Ankur Sharma <ankur.101045@gmail.com>
2021-06-20 15:32:38 +0530

    corrected response

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Mercury = require('@postlight/mercury-parser');
 let port = process.env.PORT || 3000;
 app.post("/image", jsonParser,(req, res) => {
     const url = req.body.url
-    Mercury.parse(url).then(result => res.send(result.lead_image_url));
+    Mercury.parse(url).then(result => res.send(result));
 
 });
 app.listen(port, () => {
