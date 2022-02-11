6e57c90e128c14914eb191a07e94d412a181404c
Ankur Sharma <ankur.101045@gmail.com>
2022-02-11 09:41:28 +0530

    rearranged cors

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var imagekit = new ImageKit({
 });
 
 let port = process.env.PORT || 3000;
-app.post("/image", Cors(corsOptionsDelegate), jsonParser, (req, res) => {
+app.post("/image", jsonParser, Cors(corsOptionsDelegate), (req, res) => {
     const url = req.body.url
     Mercury.parse(url).then(result => res.send(result));
 
