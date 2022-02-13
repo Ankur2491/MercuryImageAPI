36fde93d3fb913479bd5029234799129faec266d
Ankur Sharma <ankur.101045@gmail.com>
2022-02-13 10:14:24 +0530

    added a function

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ var bodyParser = require('body-parser')
 var ImageKit = require("imagekit");
 var jsonParser = bodyParser.json()
 app.use(jsonParser)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 var allowlist = ['https://hacker-board.herokuapp.com']
 var corsOptionsDelegate = function (req, callback) {
     var corsOptions;
